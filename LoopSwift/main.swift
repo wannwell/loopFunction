513b309b8969acef415d2a5c5487a37d34685de2
ตัว ป่วน <anawatsembua@macbook-pro.local>
2016-05-09 14:20:27 +0700

    Initial Commit

---
+++ LoopSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  LoopSwift
+//
+//  Created by Anawat Sembua on 5/9/2559 BE.
+//  Copyright © 2559 Anawat Sembua. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
